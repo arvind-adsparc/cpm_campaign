bd6a4f3002e7f501a3bfeb7bbade6408289803ae
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-09-08 19:46:33 +0530

    addition of tailwind css

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>CPM Database API</h1>
+      <h1 className="text-3xl font-bold underline">CPM Database API</h1>
     </div>
   );
 }
