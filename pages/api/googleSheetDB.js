6eef61c6237162796659d1e3c4e193aa521d4e9d
Arvind M <marketing.tech@adsparc.com>
2022-10-07 11:54:43 +0530

    google sheet to mongodb dump api implementation

--- pages/api/googleSheetDB.js
+++ pages/api/googleSheetDB.js
@@ -1,6 +1,9 @@
 import NextCors from "nextjs-cors";
 // import moment from "moment";
 import { google } from "googleapis";
+import dbConnect from "../../lib/dbConnect";
+
+import CPMDatabase from "../../models/CPMDatabase";
 
 export default async function handler(req, res) {
   const { method } = req;
@@ -12,7 +15,7 @@ export default async function handler(req, res) {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
-  // await dbConnect();
+  await dbConnect();
 
   switch (method) {
     case "GET":
@@ -67,6 +70,73 @@ export default async function handler(req, res) {
         // console.log(modifiedData);
 
         res.status(201).json({ success: true, data: modifiedData });
+      } catch (err) {
+        console.log("getting this far 2", err);
+
+        res.status(400).json({ error: err });
+      }
+      break;
+    case "POST":
+      const auth = new google.auth.GoogleAuth({
+        credentials: {
+          client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+          client_id: process.env.CLIENT_ID,
+          private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(
+            /\\n/g,
+            "\n"
+          ),
+        },
+        scopes: [
+          "https://www.googleapis.com/auth/drive",
+          "https://www.googleapis.com/auth/drive.file",
+          "https://www.googleapis.com/auth/spreadsheets",
+        ],
+      });
+
+      const sheets = google.sheets({
+        auth,
+        version: "v4",
+      });
+
+      const response = await sheets.spreadsheets.values.get({
+        spreadsheetId: process.env.SPREADSHEET_ID,
+        range: "A:L",
+      });
+
+      let dataSchema = [
+        "inventoryType",
+        "partner",
+        "appName",
+        "gamingInventoryType",
+        "categories",
+        "appstore",
+        "adFormat",
+        "appId",
+        "adUnitName",
+        "publisherNetPrice",
+        "gamCpm",
+        "adSparcMarignMedia",
+      ];
+
+      const dataToSend = [];
+
+      response.data.values.forEach((info, index, arr) => {
+        if (index == 0) {
+        } else {
+          let doc = {};
+          info.forEach((item, itemIndex) => {
+            doc[dataSchema[itemIndex]] = item;
+          });
+          dataToSend.push(doc);
+          // if (index < 37) {
+
+          // }
+        }
+      });
+      try {
+        // const entry = await CPMDatabase.create(JSON.parse(req.body));
+        const entry = await CPMDatabase.create(dataToSend);
+        res.status(201).json({ success: true, data: dataToSend });
       } catch (err) {
         console.log("getting this far 2", err);
 
